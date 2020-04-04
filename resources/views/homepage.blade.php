15309edf23ec604bb5173dcccb282346f31a7951
evinoguz <36725347+evinoguz@users.noreply.github.com>
2020-04-04 17:55:32 +0300

    hafta1 raporu

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.master')
+@section('contents')
+    <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header" style="background-image: url('img/indir9.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="site-heading">
+                        <h1 class="post-title">Seyahat Blog</h1>
+                        <hr class="small">
+                        <span class="subheading">Her gezen kaybolmaz...</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+
+
+            </div>
+        </div>
+    </div>
+@endsection
