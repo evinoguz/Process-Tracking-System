d5e7cafd0c2c86e01f8f9b8d431f6e647cf95880
evinoguz <36725347+evinoguz@users.noreply.github.com>
2020-04-07 01:55:44 +0300

    hafta3

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+Auth::routes();
+Route::get('/exit', function () {
+    \Illuminate\Support\Facades\Auth::logout();
+    return redirect()->route('login');
+})->name('log_out');
 Route::get('/','HomeController@index')->name('index');
 Route::get('/home','HomeController@index')->name('home');
-
-
-Auth::routes();
-
